d10de106eae96d1d48d80e961ae58b4f56f5f9a4
olygood <olivier.huttmacher@gmail.com>
2019-03-24 15:19:09 +0100

    edit link npm

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ figlet.text("                              NPX V-CARD", (err, data) => {
     console.log(chalk.green.bold(data) + "\n" + box(chalk.blue.bold("                        olygood \n")+
     chalk.blue.bold("                  Web Developer @Becode \n \n")+
     chalk.red.underline("GitHub:") + chalk.white (" https://github.com/olygood\n\n") +
-    chalk.red.underline("Npm:") + chalk.white (" https://www.npmjs.com/olygood\n\n")+
+    chalk.red.underline("Npm:") + chalk.white (" https://www.npmjs.com/~olygood\n\n")+
     chalk.red.underline("LinkedIn:") + chalk.white(" https://www.linkedin.com/in/olivier-huttmacher/\n\n") +
     chalk.red.underline("Email:") + chalk.white (" olivier.huttmacher@gmail.com"), 
     {
